5aa8c981961baaa9b65df8a2ac4946f9f6bbe37b
leebinibini <qpdjvn0601@naver.com>
2025-03-12 23:41:32 +0900

    [level 0] Title: 약수 구하기, Time: 0.19 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> divisors = new ArrayList<>(); 
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i); 
+            }
+        }
+        
+       
+        int[] answer = new int[divisors.size()];
+        for (int i = 0; i < divisors.size(); i++) {
+            answer[i] = divisors.get(i);
+        }
+        
+        return answer; 
+    }
+}
